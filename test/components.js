5ba29c9ef8946e77e4a9847d3e6aff25985d25ff
Mike Morici <Michael.Morici@cfpb.gov>
2014-04-08 10:14:01 -0400

    Updates components to test.

--- test/components.js
+++ test/components.js
@@ -23,8 +23,10 @@ describe('Components', function(){
   // changed/added in `tasks/options` is incompatible with a component.
   zips = [
     'https://github.com/cfpb/cf-buttons/archive/gh-pages.zip',
-    'https://github.com/cfpb/cf-typography/archive/gh-pages.zip',
-    'https://github.com/cfpb/cf-forms/archive/gh-pages.zip'
+    'https://github.com/cfpb/cf-forms/archive/gh-pages.zip',
+    'https://github.com/cfpb/cf-icons/archive/gh-pages.zip',
+    'https://github.com/cfpb/cf-pagination/archive/gh-pages.zip',
+    'https://github.com/cfpb/cf-typography/archive/gh-pages.zip'
   ];
 
   // Kill the `components` dir so we can start fresh

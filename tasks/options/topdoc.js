8ce55585951853c56940b3d5935fdc87043c2b14
Mike Morici <Michael.Morici@cfpb.gov>
2014-08-05 18:12:10 -0400

    Sets the new docs template as the default for the docs topdoc task.

--- tasks/options/topdoc.js
+++ tasks/options/topdoc.js
@@ -23,7 +23,7 @@ module.exports = {
     options: {
       source: 'docs/static/css/',
       destination: 'docs/',
-      template: 'node_modules/cf-component-demo/' + ( grunt.option('tpl') || 'code_examples' ) + '/',
+      template: 'node_modules/cf-component-demo/' + ( grunt.option('tpl') || 'docs' ) + '/',
       templateData: {
         jsBody: '<%= opt.jsBody %>',
         family: '<%= pkg.name %>',

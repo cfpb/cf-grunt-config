de6c0378d5393caec38833ceaf756c93ddbd3298
Scott Cranfill <scott.cranfill@cfpb.gov>
2015-10-13 13:33:27 -0400

    Use actually tested and working path, now

--- tasks/options/less.js
+++ tasks/options/less.js
@@ -8,7 +8,7 @@ module.exports = {
       sourceMapRootpath: '/',
       modifyVars: {
         baseurl: '',
-        'cf-icon-path': '../'
+        'cf-icon-path': '\'../fonts\''
       }
     },
     files: {
